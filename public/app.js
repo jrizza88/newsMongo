cf95bef1cf0bf6d4a01c4a331b0a434ce27cc7ef
jrizza88 <jetorres88@gmail.com>
2016-09-18 18:19:12 -0400

    updates

--- public/app.js
+++ public/app.js
@@ -4,8 +4,7 @@ $.getJSON('/articles', function(data) {
 for (var i = 0; i < data.length; i++) {
 
   // display the apropos information on the page
-  $('#articles').append('<p> data-id"' + data[i]._id'">' +data[i].title
-    + data[i].link + '</p>');
+  $('#articles').append('<p> data-id"' + data[i]._id + '">' + data[i].title + '<br />' + data[i].link + '</p>');
   }
     
 });
